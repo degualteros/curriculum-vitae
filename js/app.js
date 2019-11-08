bb9714767c5cc73cbb393e9c832182ad67058f1a
David Eduardo Gualteros <degualteros@poligran.edu.co>
2019-11-08 11:59:51 -0500

    Primer Commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    nombre: "David Eduardo Gualteros Gualtero",
+    skills: [
+      {nombre: "Java", icono: "https://image.flaticon.com/icons/svg/226/226777.svg"}, 
+      {nombre: "JavaScript", icono: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/245px-Unofficial_JavaScript_logo_2.svg.png"},
+      {nombre: "CSS", icono: "https://image.flaticon.com/icons/svg/919/919826.svg"},
+      {nombre: "HTML 5", icono: "https://image.flaticon.com/icons/svg/174/174854.svg"},
+      {nombre: "Python", icono: "https://image.flaticon.com/icons/svg/1822/1822899.svg"},
+      {nombre: "MySQL", icono: "https://image.flaticon.com/icons/svg/1199/1199128.svg"},
+      {nombre: "Vue jS", icono: "https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg"},
+      {nombre: "WordPress", icono: "https://image.flaticon.com/icons/svg/174/174881.svg"},
+      {nombre: "Git", icono: "https://www.stickpng.com/assets/images/5847f981cef1014c0b5e48be.png"},
+      {nombre: "GitHub", icono: "http://iconsetc.com/icons-watermarks/simple-yellow/bfa/bfa_brands-github-square/bfa_brands-github-square_simple-yellow_512x512.png"}
+    ],
+    derechos: "Creado por David G. 2019"
+  }  
+});
